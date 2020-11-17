27b30fc578cf60ebcb65eb98f6b21eeff5f4f2a2
VictorAlvaradoAraujo <viosalva@espol.edu.ec>
2020-11-16 21:33:30 -0500

    modifiacion de tokens y funciones

--- lexer.py
+++ lexer.py
@@ -2,54 +2,45 @@ import ply.lex as lex
 
 
 
-# token hechos por --> Victor Alvarado
+
 token = (
     'INT', 'DOUBLE', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'TAB', 'newline',
-    'EQUALS', 'DOSPUNTOS', 'COMA',
+    'EQUALS', 'DOSPUNTOS', 'COMA','STRING'
     'PUNTO','PUNTOS','CADENA_DE_CARACTERES', 'EQUAL', 'NOTEQ', 'LARGE', 'SMALL', 'LRGEQ', 'SMLEQ', 'ID', 'FALSO',
     'VERDAD', 'IPAR', 'DPAR', "INDENT","Int",'Double','Boolean','AND','NOT','OR','NEGATION','ILLAVE','DLLAVE',
-    'VAL','VAR','LISTOF',"SETOF","MAPOF"
+    'VAL','VAR'
 )
 
-
-# Palabras reservadas hechos por --> Paula Benites
-reserveda = ('ELSE', 'IF', 'RETURN', 'PRINT','println', 'WHILE', 'FUN','IN','FOR','STEP',
-             'TO','READLINE','SIZE','ISEMPTY','KEYS','VALUES')
-
+reserveda= ('ELSE','IF','RETURN','PRINT','println', 'WHILE','FOR','FUN',"valius")
 
 tokens=token+reserveda
-
-# Regular expression rules for simple tokens hechas por -- > Paula Benites
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
-t_IPAR = r'\('
-t_DPAR = r'\)'
-t_ILLAVE=r'\{'
-t_DLLAVE=r'\}'
-t_EQUALS = r'='
+# Regular expression rules for simple tokens
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_IPAR  = r'\('
+t_DPAR  = r'\)'
+t_EQUALS  = r'='
 t_DOSPUNTOS = r':'
-t_PUNTO = r'\.'
-t_PUNTOS = r'\.\.'
-t_COMA = r'\,'
+t_PUNTO     =r'\.'
+t_COMA      =r'\,'
 t_CADENA_DE_CARACTERES = r'(\".*\"|[a-zA-Z]+\s*)'
-# t_CADENA_DE_CARACTERES = r'(\"[^["]]*\"|[a-zA-Z]+\s*)'
-
-t_TAB = r'\t'
-t_EQUAL = r'\=\='
-t_NOTEQ = r'\!\='
-t_LARGE = r'\>'
-t_SMALL = r'\<'
-t_LRGEQ = r'\>\='
-t_SMLEQ = r'\<\='
+t_TAB       =r'\t'
+t_ignore_COMMENT = r'\/\/.*'
+t_EQUAL   = r'\=\='
+t_NOTEQ   = r'\!\='
+t_LARGE   = r'\>'
+t_SMALL   = r'\<'
+t_LRGEQ   = r'\>\='
+t_SMLEQ   = r'\<\='
 t_AND = r'\&\&'
 t_OR  = r'\|\|'
 t_NEGATION=r'\!'
 
 
 # Reglas de correspondencia
-# funciones para palabra reservada --> Scarlet espinoza excepto las estructuras de control yo hice while
+
 def t_DOUBLE(t):
     r'\d+\.\d+'
     t.value = float(t.value[::])
@@ -68,10 +59,22 @@ def t_Int(token):
     return token
 
 
+def t_STRING(token):
+    r'String'
+    token.value = str(token.value)
+    return token
+
 def t_Double(token):
     r'Double'
     token.value = str(token.value)
     return token
+
+
+def t_DOUBLE(t):
+    r'\d+\.\d+'
+    t.value = float(t.value[::])
+    return t
+
 
 def t_Boolean(token):
     r'Boolean'
@@ -103,7 +106,7 @@ def t_VAR(token):
 
 
 def t_PRINT(token):
-    r'print'
+    r'print$'
     token.value = str(token.value)
     return token
 
@@ -116,88 +119,6 @@ def t_println(token):
 
 def t_RETURN(token):
     r'return'
-    token.value = str(token.value)
-    return token
-
-def t_FUN(token):
-    r'fun'
-    token.value = str(token.value)
-    return token
-
-# Estructura de control FOR --> Victor Alvarado
-def t_FOR(token):
-    r'for'
-    token.value = str(token.value)
-    return token
-
-def t_STEP(token):
-    r'step'
-    token.value = str(token.value)
-    return token
-
-def t_TO(token):
-    r'to'
-    token.value = str(token.value)
-    return token
-def t_SIZE(token):
-    r'size'
-    token.value = str(token.value)
-    return token
-def t_ISEMPTY(token):
-    r'isEmpty'
-    token.value = str(token.value)
-    return token
-
-def t_READLINE(token):
-    r'readLine'
-    token.value = str(token.value)
-    return token
-
-def t_IN(token):
-    r'in'
-    token.value = str(token.value)
-    return token
-
-# Paula Benites estructura de control if y else
-def t_IF(token):
-    r'if'
-    token.value = str(token.value)
-    return token
-
-def t_ELSE(token):
-    r'else'
-    r'else'
-    token.value = str(token.value)
-    return token
-
-# Estructura de control WHILE -->  Scarlet Espinoza
-def t_WHILE(token):
-    r'while'
-    token.value = str(token.value)
-    return token
-
-def t_LISTOF(token):
-    r'listOf'
-    token.value = str(token.value)
-    return token
-
-def t_SETOF(token):
-    r'setOf'
-    token.value = str(token.value)
-    return token
-
-def t_MAPOF(token):
-    r'mapOf'
-    token.value = str(token.value)
-    return token
-
-def t_KEYS(token):
-    r'keys'
-    token.value = str(token.value)
-    return token
-
-def t_VALUES(token):
-    r'values'
     token.value = str(token.value)
     return token
 
@@ -216,16 +137,33 @@ def t_whitespace(token):
     r'\s+'
     pass
 
-# Error handling rule
-def t_error(token):
-    print("Error caracter no definido en token:'%s'" % token.value[0])
-    token.lexer.skip(1)
+def t_FUN(token):
+    r'fun'
+    token.value = str(token.value)
+    return token
+
+def t_FOR(token):
+    r'for'
+    token.value = str(token.value)
+    return token
+def t_STEP(token):
+    r'step'
+    token.value = str(token.value)
+    return token
+
+def t_IN(token):
+    r'in'
+    token.value = str(token.value)
+    return token
+def t_valius(token):
+    r'valius'
+    token.value = str(token.value)
     return token
 
 
-# Codigo para leer Archivo --> Victor Alvarado
+# Codigo para leer Archivo
 
-file =open("EjemplosAvance.txt", "r")
+file =open("codigo.txt", "r")
 if file.mode=="r":
     datos=file.read()
 
