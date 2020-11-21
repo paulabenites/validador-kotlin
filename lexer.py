76b8332d79c9be4abc31c322a9b563bb99e44eff
Scarlet Angelina Espinoza Moreno <scanespi@espol.edu.ec>
2020-11-20 23:16:00 -0500

    Reestructurando y agregando nuevas palabras reservadas

--- lexer.py
+++ lexer.py
@@ -2,34 +2,39 @@ import ply.lex as lex
 
 # Definicion de palagbras reservada ---> Paula Benites
 reservada= {
-            'print' : "PRINT",'println' : "println",'readline' : "readline",
-            'pair' : "pair",
-            'isEmpty' : "isEmpty",
-            "values" : "values", "keys" : "keys",
-            'else' : "else", "if" : "if",
-            'while' : "while",
-           'for' : "for",
-           'fun' : "fun", 'return' : "return", 'to' : "to"
+            'var':"VAR",'val':"VAL",
+            'Int':"INT",'Double':"DOUBLE",'Boolean':"BOOLEAN", 'String':"STRING",
+            'true':"TRUE",'false':"FALSE",
+            'print' : "PRINT",'println' : "PRINTLN",'readline' : "READLINE",
+            'isEmpty' : "ISEMPTY",'size':"SIZE",'slice':"SLICE",
+            "values" : "VALUES", "keys" : "KEYS",
+            'else' : "ELSE", "if" : "IF",
+            'while' : "WHILE",
+            'for' : "FOR", 'step':"STEP",'in':"IN",
+            'fun' : "FUN", 'return' : "RETURN", 'to' : "TO",
+            'listOf':"LISTOF",'setOf':"SETOF",'mapOf':"MAPOF",'pair' : "PAIR",
+            'first':"FIRST",'capitalize':"CAPITALIZE"
 }
 
 
 # Definicion de las lista de tokens --> Victor Alvardado
 tokens = [
-    'INT', 'DOUBLE', 'FALSO', 'VERDAD', 'ID', 'CADENA_DE_CARACTERES',
+    'ENTERO', 'DECIMAL', 'ID', 'CADENA_DE_CARACTERES',
     'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
-    'EQUALS', 'DOSPUNTOS', 'COMA','STRING','PUNTO','PUNTOS', 'EQUAL',
+    'EQUALS', 'DOSPUNTOS', 'COMA','PUNTO','PUNTOS', 'EQUAL',
     'LARGE', 'SMALL', 'NOTEQ', 'LRGEQ', 'SMLEQ',
-    'ILLAVE','DLLAVE', 'IPAR', 'DPAR',
+    'ILLAVE','DLLAVE', 'IPAR', 'DPAR','ICOR','DCOR',
     'AND','NOT','OR','NEGATION',
     "INDENT", 'newline','TAB',
-    'LISTOF',"SETOF","MAPOF", "STEP","IN", 'VAL','VAR', "Int",'Double','Boolean',
+
 ] + list(reservada.values())
 
-# tokens=token+reserveda
 # Regular expression rules for simple tokens -- Paula Benites
 t_PLUS    = r'\+'
 t_ILLAVE    = r'\{'
 t_DLLAVE    = r'\}'
+t_ICOR    = r'\['
+t_DCOR    = r'\]'
 t_MINUS   = r'-'
 t_TIMES   = r'\*'
 t_DIVIDE  = r'/'
@@ -39,7 +44,7 @@ t_EQUALS  = r'='
 t_DOSPUNTOS = r':'
 t_PUNTO     =r'\.'
 t_COMA      =r'\,'
-t_CADENA_DE_CARACTERES = r'(\".*\"|[a-zA-Z]+\s*)'
+t_CADENA_DE_CARACTERES = r"\"[a-zA-Z0-9\s]*\"|'[a-zA-Z0-9\s]*'"
 t_TAB       =r'\t'
 t_ignore_COMMENT = r'\/\/.*'
 t_EQUAL   = r'\=\='
@@ -52,110 +57,18 @@ t_AND = r'\&\&'
 t_OR  = r'\|\|'
 t_NEGATION=r'\!'
 
-# t_VAR= r'var'
-# t_VAL= r'val'
-
-
 # definicion de la funciones exectp estructuras de control -- > Scarlet Espinoza
 # Reglas de correspondencia
 
-def t_VAR(token):
-    r'var\s'
-    token.value = str(token.value)
-    return token
-
-def t_VAL(token):
-    r'val\s'
-    token.value = str(token.value)
-    return token
-
-def t_INT(t):
+def t_ENTERO(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-def t_Int(token):
-    r'Int'
-    token.value = str(token.value)
-    return token
-
-
-def t_STRING(token):
-    r'String=\String\s'
-    token.value = str(token.value)
-    return token
-
-
-
-
-def t_Double(token):
-    r'Double=|Double\s'
-    token.value = str(token.value)
-    return token
-
-
-def t_DOUBLE(t):
+def t_DECIMAL(t):
     r'\d+\.\d+'
     t.value = float(t.value[::])
     return t
-
-
-def t_Boolean(token):
-    r'Boolean=|Boolean\s'
-    token.value = str(token.value)
-    return token
-
-
-def t_VERDAD(token):
-    r'true\s'
-    token.value = str(token.value)
-    return token
-
-
-def t_FALSO(token):
-    r'false\s'
-    token.value = str(token.value)
-    return token
-
-
-def t_SIZE(token):
-    r'size\s'
-    token.value = str(token.value)
-    return token
-
-#Paula Benites hice el If y Else
-
-
-#Paula Benites hice listof
-def t_LISTOF(token):
-    r'listOf'
-    token.value = str(token.value)
-    return token
-
-# SCARLET ESPINOZA HICE EL SET OF 
-def t_SETOF(token):
-    r'setOf'
-    token.value = str(token.value)
-    return token
-#VICTOR ALVARADO HICE EL MAPOF
-def t_MAPOF(token):
-    r'mapOf'
-    token.value = str(token.value)
-    return token
-
-
-def t_STEP(token):
-    r'step\s'
-    token.value = str(token.value)
-    return token
-
-
-def t_IN(token):
-    r'in\s'
-    token.value = str(token.value)
-    return token
-
-
 
 def t_ID(token):
     '([a-zA-Z_][a-zA-Z0-9_]*)'
@@ -179,9 +92,9 @@ def t_error(token):
     return token
 
 
-# Codigo para leer Archivo --> Victor Alvarado
+# Codigo para leer Archivo
 
-file =open("EjemplosAlvarado.txt", "r")
+file =open("EjemplosBenites.txt", "r")
 if file.mode=="r":
     datos=file.read()
 
